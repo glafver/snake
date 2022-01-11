bce2330f083568b9ddc7b33388890660b55b38fb
glafver <glafver22@gmail.com>
2022-01-11 19:08:20 +0100

    Game is ready

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 let gameView = document.querySelector('#gameView');
 let ctx = gameView.getContext('2d');
 
-let snakeVelocity = 500;
-
-let headX = 100;
-let headY = 100;
-let snakeSize = 1;
-let snakeBody = [];
-
 const headSize = 20;
 
-let direction = 'right';
-let lastUpdTime = 0;
+let headX;
+let headY;
+let snakeSize;
+let snakeBody;
+
+let appleX;
+let appleY;
+
+let direction;
+let lastUpdTime;
+let snakeVelocity;
 
 const getRndInteger = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let appleX = getRndInteger(0, 20) * headSize;
-let appleY = getRndInteger(0, 20) * headSize;
+const initGame = function() {
+    direction = 'right';
+    lastUpdTime = 0;
+    snakeVelocity = 300;
+
+    snakeSize = 2;
+    snakeBody = [];
+    headX = getRndInteger(0, 20) * headSize;
+    headY = getRndInteger(0, 20) * headSize;
+
+    appleX = getRndInteger(0, 20) * headSize;
+    appleY = getRndInteger(0, 20) * headSize;
+}
 
 const playGame = function() {
 
@@ -73,6 +86,12 @@ const playGame = function() {
         snakeBody.pop();
     }
 
+    for (let i = 1; i < snakeBody.length; i++) {
+        if (snakeBody[i].x === headX && snakeBody[i].y === headY) {
+            initGame();
+        }
+    }
+
     ctx.fillStyle = 'red';
     ctx.fillRect(appleX, appleY, headSize - 1, headSize - 1);
 
@@ -81,31 +100,27 @@ const playGame = function() {
         ctx.fillRect(e.x, e.y, headSize - 1, headSize - 1);
     })
 
-
-
-
-
     window.requestAnimationFrame(playGame);
 }
 
-
 document.addEventListener('keydown', function(e) {
     // left
-    if (e.which === 37) {
+    if (e.which === 37 && direction !== 'right') {
         direction = 'left';
     }
     // up
-    else if (e.which === 38) {
+    else if (e.which === 38 && direction !== 'down') {
         direction = 'up';
     }
     // right
-    else if (e.which === 39) {
+    else if (e.which === 39 && direction !== 'left') {
         direction = 'right';
     }
     // down
-    else if (e.which === 40) {
+    else if (e.which === 40 && direction !== 'up') {
         direction = 'down';
     }
 })
 
+initGame();
 window.requestAnimationFrame(playGame);
