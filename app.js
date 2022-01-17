3a5b8e3b3888791967677e8552d105a48b103f0b
glafver <glafver22@gmail.com>
2022-01-17 21:56:20 +0100

    Chaged game viewport depending on window size

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 let gameView = document.querySelector('#gameView');
-let ctx = gameView.getContext('2d');
 
-const headSize = 20;
+let ctx = gameView.getContext('2d');
+let windowSize;
+let headSize;
+
+const totalElementsAmount = 20;
 
 let headX;
 let headY;
@@ -15,6 +18,23 @@ let direction;
 let lastUpdTime;
 let snakeVelocity;
 
+const rescaleGameView = function() {
+    let type;
+
+    if (window.innerWidth > innerHeight) {
+        windowSize = window.innerHeight * 0.85;
+        type = 'vertical';
+    } else {
+        windowSize = window.innerWidth * 0.95;
+        type = 'horizontal';
+    }
+    gameView.width = windowSize;
+    gameView.height = windowSize;
+    headSize = windowSize / totalElementsAmount;
+    // headSize = Math.trunc(windowSize / totalElementsAmount);
+    console.log(type, window.innerWidth, window.innerHeight, headSize);
+};
+
 const getRndInteger = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -26,11 +46,11 @@ const initGame = function() {
 
     snakeSize = 2;
     snakeBody = [];
-    headX = getRndInteger(0, 20) * headSize;
-    headY = getRndInteger(0, 20) * headSize;
+    headX = getRndInteger(0, totalElementsAmount) * headSize;
+    headY = getRndInteger(0, totalElementsAmount) * headSize;
 
-    appleX = getRndInteger(0, 20) * headSize;
-    appleY = getRndInteger(0, 20) * headSize;
+    appleX = getRndInteger(0, totalElementsAmount) * headSize;
+    appleY = getRndInteger(0, totalElementsAmount) * headSize;
 }
 
 const playGame = function() {
@@ -43,7 +63,7 @@ const playGame = function() {
     }
 
     lastUpdTime = currentTime;
-    ctx.clearRect(0, 0, 400, 400);
+    ctx.clearRect(0, 0, windowSize, windowSize);
     if (direction === 'left') {
         headX -= headSize;
         if (headX < 0) {
@@ -72,9 +92,9 @@ const playGame = function() {
         }
     }
 
-    if (headX === appleX && headY === appleY) {
-        appleX = getRndInteger(0, 20) * headSize;
-        appleY = getRndInteger(0, 20) * headSize;
+    if (Math.abs(headX - appleX) < 2 && Math.abs(headY - appleY) < 2) {
+        appleX = getRndInteger(0, totalElementsAmount) * headSize;
+        appleY = getRndInteger(0, totalElementsAmount) * headSize;
         snakeSize++;
         snakeVelocity -= 10;
     }
@@ -87,7 +107,7 @@ const playGame = function() {
     }
 
     for (let i = 1; i < snakeBody.length; i++) {
-        if (snakeBody[i].x === headX && snakeBody[i].y === headY) {
+        if (Math.abs(snakeBody[i].x - headX) < 2 && Math.abs(snakeBody[i].y - headY) < 2) {
             initGame();
         }
     }
@@ -105,22 +125,41 @@ const playGame = function() {
 
 document.addEventListener('keydown', function(e) {
     // left
-    if (e.which === 37 && direction !== 'right') {
+    if (e.code === 'ArrowLeft' && direction !== 'right') {
         direction = 'left';
     }
     // up
-    else if (e.which === 38 && direction !== 'down') {
+    else if (e.code === 'ArrowUp' && direction !== 'down') {
         direction = 'up';
     }
     // right
-    else if (e.which === 39 && direction !== 'left') {
+    else if (e.code === 'ArrowRight' && direction !== 'left') {
         direction = 'right';
     }
     // down
-    else if (e.which === 40 && direction !== 'up') {
+    else if (e.code === 'ArrowDown' && direction !== 'up') {
         direction = 'down';
     }
 })
 
+document.querySelector('.joystick').addEventListener('click', function(e) {
+    // left
+    if (e.target.id === 'btn--left' && direction !== 'right') {
+        direction = 'left';
+    }
+    // up
+    else if (e.target.id === 'btn--up' && direction !== 'down') {
+        direction = 'up';
+    }
+    // right
+    else if (e.target.id === 'btn--right' && direction !== 'left') {
+        direction = 'right';
+    }
+    // down
+    else if (e.target.id === 'btn--down' && direction !== 'up') {
+        direction = 'down';
+    }
+})
+rescaleGameView();
 initGame();
 window.requestAnimationFrame(playGame);
